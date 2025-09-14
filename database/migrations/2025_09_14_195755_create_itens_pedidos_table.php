865f316b4cdfb0e66d8f8b4c5ae87f992b5b119a
marchesido <marchesido@gmail.com>
2025-09-14 19:49:53 -0300

    migration pedido & item pedido

---
+++ database/migrations/2025_09_14_195755_create_itens_pedidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itens_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantidade');
+            $table->decimal('preco',10,2);
+            $table->foreignId('pedido_id')->constrained()->OnDelete('cascade');
+            $table->foreignId('produto_id')->constrained()->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_pedidos');
+    }
+};
