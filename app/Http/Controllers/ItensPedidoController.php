9e2f24b465c760c7b30d4dfb577336f033145569
marchesido <marchesido@gmail.com>
2025-09-14 19:52:01 -0300

    add controllers  pedidos & itenspedidos

---
+++ app/Http/Controllers/ItensPedidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\itensPedido;
+use Illuminate\Http\Request;
+
+class ItensPedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\itensPedido  $itensPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(itensPedido $itensPedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\itensPedido  $itensPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(itensPedido $itensPedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\itensPedido  $itensPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, itensPedido $itensPedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\itensPedido  $itensPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(itensPedido $itensPedido)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use App\Models\Pedido;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pedidos = Pedido::all();
+        return view('pedidos.index',compact('pedidos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $users = User::all();
+        return view('pedidos.create', compact('users'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'status' => 'required|string|max:255',
+            'user_id' => 'required|integer',
+        ]);
+
+        Pedido::create($request->all());
+
+        return redirect()->route('pedidos.index')
+                         ->with('success', 'Pedido criado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pedido $pedido)
+    {
+         $users = User::all();
+        return view('pedidos.edit', compact('pedido','users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        $request->validate([
+            'status' => 'required|string|max:255',
+            'user_id' => 'required|integer',
+        ]);
+
+        $pedido->update($request->all());
+
+        return redirect()->route('pedidos.index')
+                         ->with('success', 'Pedido atualizado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pedido $pedido)
+    {
+        $pedido->delete();
+
+        return redirect()->route('produtos.index')
+                         ->with('success', 'Produto excluído com sucesso!');
+    }
+}
