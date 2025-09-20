8f5cdb10e7c04185ccdf97f1a3ec4b672bedc87f
marchesido <marchesido@gmail.com>
2025-09-20 17:45:14 -0300

    ajusteCard

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,11 +1,11 @@
 <div class="col">
     <div class="card h-100">
-        <img src="{{ $produto ['image'] }}" class="produto-img-top" alt="{{ $produto [ 'title' ] }}" style="object-fit: fill;">
+        <img src="{{ $produto -> image }}" class="produto-img-top" alt="{{ $produto -> nome }}" style="object-fit: fill;">
         <div class="produto-body">
-            <h5 class="produto-title">{{ $produto [ 'title' ] }}</h5>
-            <p class="produto-text">{{ $produto ['description'] }}</p>
-            <p class="produto-text">{{ $produto ['price'] }}</p>
-            <a href="{{ route('produtos.show', $produto ['id']) }}" class="btn btn-primary">Ver mais</a>
+            <h5 class="produto-title">{{ $produto -> nome }}</h5>
+            <p class="produto-text">{{ $produto -> descricao }}</p>
+            <p class="produto-text">{{ $produto -> preco }}</p>
+            <a href="{{ route('produtos.show', $produto -> id) }}" class="btn btn-primary">Ver mais</a>
         </div>
     </div>
 </div>
