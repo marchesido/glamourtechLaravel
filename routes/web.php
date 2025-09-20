4dbac7233fd48bdfef2d89d36433718b6fd9e648
marchesido <marchesido@gmail.com>
2025-09-20 17:37:44 -0300

    routeCommit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItensPedidoController;
 use App\Http\Controllers\PedidoController;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +18,20 @@ use App\Http\Controllers\ProdutoController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('produtos', ProdutoController::class);
 Route::resource('categorias', CategoriaController::class);
 Route::resource('pedidos', PedidoController::class);
 Route::resource('itens-pedidos', ItensPedidoController::class);
+
+Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
+Route::post('/contato', [ContatoController::class, 'store'])->name('contato.store');
+
+
+Route::get('/sobre', function () {
+    return view('sobre');
+})->name('sobre');
